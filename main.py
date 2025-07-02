62da381c995a8b5fd6d89052ad4563fb2d456310
YuryDziatko <yurydziatko@yahoo.com>
2025-07-02 12:36:56 -0500

    Update main.py
    Update to web service

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-from fastapi import FastAPI
-from stock_logic import get_prediction_for_random_stock
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+
+
+import pandas as pd
+from ModelPrediction import get_train_test_data, evaluate_classification_models
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def root():
-    return {"message": "Welcome to the Stock Predictor API!"}
+@app.get("/", response_class=HTMLResponse)
+def show_form(request: Request):
+    df_stocks = pd.read_json('Stocks_name.json')
+    tickers = df_stocks["symbol"].tolist()
+    return templates.TemplateResponse("index.html", {"request": request, "tickers": tickers})
 
-@app.get("/predict")
-def predict_stock():
+
+@app.post("/api/predict")
+def api_predict(ticker: str = Form(...)):
     try:
-        prediction = get_prediction_for_random_stock()
-        return prediction
-    except ValueError:
-        return {"error": "Prediction failed, try another stock."}
+        X_train, X_test, Y_train, Y_test = get_train_test_data(ticker)
+        result = evaluate_classification_models(X_train, X_test, Y_train, Y_test, ticker)
+
+        if result:
+            output_t=f"Stock {ticker} will increase tomorrow"
+        else:
+            output_t = f"Stock {ticker} will not increase tomorrow"
+
+        return JSONResponse(content={"success": True, "message": output_t})
+
+    except Exception as e:
+        return JSONResponse(content={"success": False, "message": "Try another stock"})
